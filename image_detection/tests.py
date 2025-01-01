8a0badbd79b0e88fca6adca94ef0cd844afeb24d
akkupy <akkupy@proton.me>
2025-01-01 22:01:39 +0530

    Added code

--- image_detection/tests.py
+++ image_detection/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import cv2
 
-# Create your tests here.
+def returnCameraIndexes():
+    # checks the first 10 indexes.
+    index = 0
+    arr = []
+    i = 10
+    while i > 0:
+        cap = cv2.VideoCapture(index)
+        if cap.read()[0]:
+            arr.append(index)
+            cap.release()
+        index += 1
+        i -= 1
+    return arr
+print(returnCameraIndexes())
